53fa992696a24d0ea8e50a853b5c88e7cdf1b258
Georgia Hoost <gfroehling@icloud.com>
2021-03-25 18:58:19 +0100

    asc desc working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,65 @@
 import './App.css';
+import React from 'react';
 import Movies from './Movies';
 import Database from './Database';
-// import Searchbar from './Searchbar';
+import { render } from "@testing-library/react";
 
-function App()
+
+class App extends React.Component
 {
-  return (
-    <div className="App">
-      <h1>The 100 best Movies Ever</h1>
-      <Movies data={Database}></Movies>
-      {/* <Searchbar></Searchbar> */}
-    </div>
-  );
+  state = {
+    Database: Database
+  }
+
+  fetch()
+  {
+    this.setState({
+      Database: Database
+    })
+  }
+
+  sortAsc(e)
+  {
+    const order = e.target.value;
+
+    if (order === 'asc') {
+      this.setState({
+        Database: Database.sort((a, b) => a.year - b.year)
+      })
+    }
+    if (order === 'desc') {
+      this.setState({
+        Database: Database.sort((a, b) => b.year - a.year)
+      })
+    }
+
+  }
+
+  render()
+  {
+    return (
+      <div className="App">
+        <h1>The 100 best Movies Ever</h1>
+        <div className="searchDiv">
+          <form>
+
+            <select onChange={e => this.sortAsc(e)}>
+              <option value="asc">Older movies first</option>
+              <option value="desc">Newer movies first</option>
+            </select>
+            {/* <button onClick={e => this.sortAsc} className="dateAsc">Sort by Date Ascending</button>
+            <button className="dateDesc">Sort by Date Descending</button> */}
+            <button className="bestRate">Best Rate</button>
+            <button className="aToZ">A - Z</button>
+            <button className="zToA">Z - A</button>
+          </form >
+        </div>
+        <Movies data={Database}></Movies>
+
+      </div>
+    );
+  }
 }
+
 
 export default App;

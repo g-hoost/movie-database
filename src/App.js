1688a998830e125a25524cdb6f083b2c642bdd4c
Georgia Hoost <gfroehling@icloud.com>
2021-03-26 17:58:55 +0100

    fetch and sort working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import Movies from './Movies';
 import Database from './Database';
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 
 
 class App extends React.Component
@@ -32,6 +32,9 @@ class App extends React.Component
         Database: Database.sort((a, b) => b.year - a.year)
       })
     }
+  }
+  sortDate()
+  {
 
   }
 
@@ -39,7 +42,7 @@ class App extends React.Component
   {
     return (
       <div className="App">
-        <h1>The 100 best Movies Ever</h1>
+        <h1>The Best Movies of All Times</h1>
         <div className="searchDiv">
           <form>
             <button onClick={e => this.fetch()}>Load all movies!</button>

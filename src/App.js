e2d63f33057704f8482d96e98aebf03862969239
Georgia Hoost <gfroehling@icloud.com>
2021-03-26 18:14:27 +0100

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Movies from './Movies';
 import Database from './Database';
 // import { render } from "@testing-library/react";
-
+//and TEst
 
 class App extends React.Component
 {
